ab32c86870c815e9841e9d7b06b4fd9f760a9f9c
almahmuddev <devalmahmud@gmail.com>
2026-07-19 12:17:51 +0600

    added categories

---
+++ client/src/lib/categories.ts
@@ -0,0 +1,10 @@
+export const CATEGORIES: string[] = [
+  'Web Development',
+  'Design',
+  'Backend Development',
+  'Content Writing',
+  'UI/UX Design',
+  'WordPress',
+  'Video Editing',
+  'Data Entry',
+]
